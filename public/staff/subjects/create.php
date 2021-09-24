03995ce9f45f0318b34104b5f1cba7bf8067ad37
elham-en <ilhamrezaie@gmail.com>
2021-09-24 22:27:52 +1000

    Use form data to create new records

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -7,10 +7,19 @@
         $position = $_POST['position'] ?? '';
         $visible = $_POST['visible'] ?? '';
 
-        echo "Form parameters<br />";
-        echo "Menu name: " . $menu_name . "<br />";
-        echo "Position: " . $position . "<br />";
-        echo "Visible: " . $visible . "<br />";
+        $query = "INSERT INTO subjects (menu_name, position, visible) ";
+        $query .= "VALUES ('" . $menu_name ."', '" . $position ."', '" . $visible ."')";
+        $result = mysqli_query($db, $query);
+        //For INSERT statements, $result is true/false
+        if ($result) { 
+            $new_id = mysqli_insert_id($db);
+            redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));
+        } else {
+            echo mysqli_error($db); //what was the problem?
+            db_disconnect($db); 
+            exit; //make sure noting else execute
+        }
+    
     } else { //if get request, means coming from the URL. Redirect back to new.php 
         redirect_to(url_for('/staff/subjects/new.php'));
     }
